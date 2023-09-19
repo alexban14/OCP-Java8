066b7849f0b8e14dc22c75332f94aac66a466353
Alex Ban <alexbanut10@gmail.com>
2023-09-19 22:01:35 +0300

    JavaBeans encapsulation standards for Java objects

---
+++ Design_Patterns-Principles/DesignPrinciples/JavaBeanClass.java
@@ -0,0 +1,23 @@
+public class JavaBeanClass {
+	private int age;
+
+	public int getAge() {
+		return age;
+	}
+
+	public boolean isBird() {
+		return bird;
+	}
+
+	public boolean getBird() {
+		return bird;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public void setNumChildren (int numChildren) {
+		this.numChildren = numChildren;
+	}
+}
