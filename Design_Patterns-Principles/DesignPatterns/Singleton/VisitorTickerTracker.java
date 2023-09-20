a43971a6d4f0b888374f534fe8a29683b19a42cc
Alex Ban <alexbanut10@gmail.com>
2023-09-20 21:02:50 +0300

    lazy instatiation of singleton objects

---
+++ Design_Patterns-Principles/DesignPatterns/Singleton/VisitorTickerTracker.java
@@ -0,0 +1,16 @@
+// Lazy instantiation
+public class VisitorTicketTracker {
+	private static VisitorTicketTracker instance;
+
+	private VisitorTicketTracker() {
+	}
+
+	public static VisitorTicketTracker getInstance() {
+		if(instance == null) {
+			instance = new VisitorTicketTracker(); // NOT THREAD-SAFE!
+		}
+		return instance;
+	}
+
+	// Data access methods...
+}
